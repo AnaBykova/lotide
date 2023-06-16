6a8c8771ef8ea4ba5786c25fd50d93cb027dd91d
AnaBykova <anabykovaca@gmail.com>
2023-06-16 16:09:35 -0600

    console.assert + emoji

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(" \u2705 Assertion Passed: " + actual + " === " + expected);
+  } else
+    console.log(" \u274C Assertion Passed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
