7a0c92a28b2f20c34459257e0b8a0d7fb32148f4
AnaBykova <anabykovaca@gmail.com>
2023-06-18 13:36:01 -0600

    compare arrays by elements

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+/*function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log("\u2705 Assertion Passed: ${actual} === ${expected}");
+  } else {
+    console.log("\u274C Assertion Failed: ${actual} !== ${expected}");
+  }
+}*/
+
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2705 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274C Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
