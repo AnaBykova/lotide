cc0b852d1978fffce67a1d3058945e7e33b5a842
AnaBykova <anabykovaca@gmail.com>
2023-06-25 16:00:30 -0600

    message correction

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -16,9 +16,9 @@ const findKeyByValue = function(object, film) {
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(" \u2705 Assertion Passed: ${actual} === ${expected}");
+    console.log(` \u2705 Assertion Passed: ${actual} === ${expected}`);
   } else
-    console.log(" \u274C Assertion Passed: ${actual} !== ${expected}");
+    console.log(` \u274C Assertion Passed: ${actual} !== ${expected}`);
 };
 
 const bestTVShowsByGenre = {
