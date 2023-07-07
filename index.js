8a45b399b3ba85547fc46ab27b5b0d7fef7c5a05
AnaBykova <anabykovaca@gmail.com>
2023-07-07 08:11:54 -0600

    export all function in index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  eqArrays: eqArrays
+};
