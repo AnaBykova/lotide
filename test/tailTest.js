dcaa17a7922e76db5cf4a2d527f4fcec1507bf8d
AnaBykova <anabykovaca@gmail.com>
2023-07-06 20:59:22 -0600

    switch assert.strictEqual to assert.deepEqual

--- test/tailTest.js
+++ test/tailTest.js
@@ -12,19 +12,16 @@ const tail = require('../tail');
 describe('#tail', () => {
   it('should return an empty array for an empty input', () => {
     const result = tail([]);
-    assert.strictEqual(result.length, 0);
+    assert.deepEqual(result, []);
   });
 
   it('should return the tail of an array with more than one element', () => {
     const result = tail([1, 2, 3, 4]);
-    assert.strictEqual(result.length, 3);
-    assert.strictEqual(result[0], 2);
-    assert.strictEqual(result[1], 3);
-    assert.strictEqual(result[2], 4);
+    assert.deepEqual(result, [2, 3, 4]);
   });
 
   it('should return an empty array for an array with one element', () => {
     const result = tail(['Hello']);
-    assert.strictEqual(result.length, 0);
+    assert.deepEqual(result, []);
   });
 });
